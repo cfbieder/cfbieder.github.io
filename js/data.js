25306d600258d3eab16e86ba9b30f44cd7a87f0f
Chris <cfbieder@Hotmail.com>
2018-07-27 19:32:27 +0200

    v1

---
+++ js/data.js
@@ -0,0 +1,116 @@
+
+var scenes = [{
+    number : 0,
+    name : "Overview by Country",
+    subtitle : "Overall Satisfaction",
+    active : true,
+    showmap : true
+}, {
+     number : 1,
+     name : "What factors are considered",
+     subtitle : "Household net financial wealth in usd",
+     active : false,
+     showmap : false
+}, {
+    number : 2,
+    name : "What factors are considered",
+    subtitle : "Labour market insecurity as pct",
+    active : false,
+    showmap : true
+},{
+    number : 3,
+    name : "What factors are considered",
+    subtitle : "Employment rate as pct",
+    active  : false,
+    showmap : true
+}, {
+    number : 4,
+    name : "What factors are considered",
+    subtitle : "Personal earnings in usd",
+    active  : false,
+    showmap : true 
+}, {
+    number : 5,
+    name : "What factors are considered",
+    subtitle : "Air pollution in ugm3",
+    active  : false,
+    showmap : true 
+}, {
+    number : 6,
+    name : "What factors are considered",
+    subtitle : "Water quality as pct",
+    active  : false,
+    showmap : true
+}, {
+    number : 7,
+    name : "What factors are considered",
+    subtitle : "Self-reported health as pct",
+    active  : false,
+    showmap : true 
+}, {
+    number : 8,
+    name : "What factors are considered",
+    subtitle : "Homicide rate as rat",
+    active  : false,
+    showmap : true    
+}]
+
+var series_pallet = [
+    ["red","yellow", "green"],
+    ['gray','yellow','green'],
+    ['green','yellow','gray'],
+    ['gray','yellow','green'],
+    ['gray','yellow','green'],
+    ['gray','yellow','green'],
+    ['gray','yellow','green'],
+    ['gray','yellow','green'],
+    ['gray','yellow','green']
+]
+
+var annotations = [
+    {page : 1, scene : 0,px : 'Finland',py:8,opacity : 1,text:"Countries in the top end of satisfaction ratings, tend to be Nothern European"},
+    {page : 2, scene : 1,px: 'USA' ,py:0,text:"USA appears to represent a significant outlyer"}
+]
+
+var scene = 
+[
+    ["South Africa",4.8,17042,26.5,43,11554,22,69,67,10  ],
+    ["Greece",5.2,18117,17.4,52,25124,18,69,74,1  ],
+    ["Portugal",5.2,31877,6.5,65,24529,10,87,46,1  ],
+    ["Hungary",5.3,23289,4.8,67,21711,19,76,56,1.2  ],
+    ["Turkey",5.5,4429,13,51,22848,20,63,66,1.7  ],
+    ["Estonia",5.6,16967,4,72,23621,8,82,51,3.1  ],
+    ["Slovenia",5.8,20048,4,66,34965,16,89,65,0.6  ],
+    ["Italy",5.9,64019,8.1,57,35397,18,71,66,0.8  ],
+    ["Japan",5.9,97595,1.5,74,39113,14,86,35,0.3  ],
+    ["South Korea",5.9,33495,2.4,66,32399,28,78,33,1.1  ],
+    ["Latvia",5.9,17105,6.8,69,22389,11,77,46,6.6  ],
+    ["Poland",6,14997,4.3,65,25921,22,80,58,0.8  ],
+    ["Russia",6,2260,3.6,70,22101,15,54,43,11.3  ],
+    ["Slovak Republic",6.1,10846,6.7,65,23508,21,82,66,0.8  ],
+    ["France",6.4,59479,5,65,42992,13,82,68,0.6  ],
+    ["Spain",6.4,35443,17.3,60,37333,11,73,72,0.6  ],
+    ["OECD - Total",6.5,90570,4.9,67,44290,14,81,69,3.6  ],
+    ["Czech Republic",6.6,24258,1.8,72,23722,20,87,61,0.8  ],
+    ["Mexico",6.6,4750,4.6,61,15311,16,67,66,17.9  ],
+    ["Brazil",6.6,7102,4.9,64,14024,10,72,70,27.6  ],
+    ["Chile",6.7,21409,8.1,62,28434,16,69,57,4.5  ],
+    ["United Kingdom",6.7,83405,2.6,74,42835,11,85,70,0.2  ],
+    ["Belgium",6.9,104084,4.8,62,49587,15,84,75,1  ],
+    ["Luxembourg",6.9,74141,3.2,66,62636,12,85,70,0.6  ],
+    ["USA",6.9,176076,3.8,69,60154,10,84,88,4.9  ],
+    ["Austria",7,59574,2.7,72,48295,16,93,70,0.4  ],
+    ["Germany",7,57358,2,75,46389,14,93,65,0.4  ],
+    ["Ireland",7,43493,2.1,65,51681,7,82,82,0.6  ],
+    ["Israel",7.2,61805,2.6,69,34023,21,67,84,1.7  ],
+    ["Australia",7.3,57462,4.3,72,52063,5,92,85,1  ],
+    ["Canada",7.3,85758,3.9,73,48403,7,91,88,1.4  ],
+    ["New Zealand",7.3,52718,4.9,76,39397,5,90,88,1.3  ],
+    ["Sweden",7.3,90708,5.7,76,42816,6,95,80,1  ],
+    ["Netherlands",7.4,90002,2.1,75,52833,14,93,76,0.6  ],
+    ["Denmark",7.5,73543,2.3,75,52580,9,94,72,0.7  ],
+    ["Finland",7.5,27972,2.7,69,42127,6,94,70,1.4  ],
+    ["Iceland",7.5,64398,2.6,86,55984,3,99,76,0.9  ],
+    ["Norway",7.5,20347,2.7,74,53643,5,96,78,0.6  ],
+    ["Switzerland",7.5,128415,1.8,80,60124,15,96,80,0.5  ]
+  ]
---
+++ js/scene_controls.js
@@ -0,0 +1,504 @@
+
+var activeScene = 0;
+
+//var btnContainer = document.getElementById("navigation");
+//var btns = btnContainer.getElementsByClassName("btn");
+
+var tooltip = d3.select("#navbar")
+ .append("div")
+ .attr("class", "toolTip")
+
+var data = [];
+var factors = [];
+var infoPoints = [];
+
+var margin = {top: 40, right: 20, bottom: 100, left: 100},
+    width = 1000 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var y = d3.scaleLinear()
+    .range([ height,0]);  
+
+
+var x = d3.scaleBand().padding(0.1)
+    .range([0,width]);
+
+var x_scatter = d3.scaleLinear()
+    .range([0,width]); 
+
+var svg = d3.select(".scene").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var svg_scatter = d3.select(".scene_scatter").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var div = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+
+var divInfo = d3.select("body").append("div")	
+    .attr("class", "infotip")				
+    .style("opacity", 0);
+
+var select_scatter = d3.select('#dropdown_scatter')
+    .append('select')
+        .attr('class','select_scatter')
+        .on('change',onchange_scatter)
+
+var select_bar = d3.select('#dropdown_bar')
+    .append('select')
+        .attr('class','select_bar')
+        .on('change',onchange_bar)
+
+createDropDownData = function() {
+    var factors = [];
+    for (i=1;i<scenes.length;i++) {
+        factors.push ({value : i, name :scenes[i].subtitle})
+    }
+    return factors
+}
+
+
+function onchange_scatter() {
+    selectValue = d3.select('.select_scatter').property('value')
+    activeScene = parseInt(selectValue);
+    transitionScatterChart();
+};
+
+function onchange_bar() {
+    selectValue = d3.select('.select_bar').property('value')
+    activeScene = parseInt(selectValue);
+    transitionBarChart();
+};
+
+
+
+
+function updateAnnotations(svg,scene){
+
+
+    var elem = svg.selectAll(".cir")
+    elem.transition()		
+        .duration(500)		
+        .style("opacity",0);
+
+    infoPoints = []
+    annotations.forEach(function (element) {
+        if (element.scene == scene) {
+            infoPoints.push(element);
+        }
+    })
+
+    
+    
+    elem = svg.selectAll(".cir").data(infoPoints)
+
+
+
+    var elemEnter = elem.enter()
+        .append("g")
+        .attr("class", "cir")
+        .attr("transform", function(d){return "translate("+x(d.px)+","+d.py+")"})
+        .style("opacity",1)
+        .on("mouseover", function(d) {
+            divInfo.transition()		
+                .duration(200)		
+                .style("opacity", .9);
+            divInfo.html("<strong>"+d.text+"</strong>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .style("display", "inline-block");
+        })
+        .on("mouseout", function(d) {		
+            divInfo.transition()		
+                .duration(500)		
+                .style("opacity", 0)	
+        });
+    
+    elem.exit().remove()
+
+    var circle = elemEnter.append("ellipse")
+        .attr("rx",20)
+        .attr("ry",10)
+        .attr("stroke","black")
+        .attr("fill", "#a1b7cc")
+        .style("opacity", .3)
+
+    elemEnter.append("text")
+        .attr("dx", -12)
+        .attr("dy", 5)
+        .text("Info")
+}
+
+
+transitionScatterChart = function() {
+    data = [];
+    
+    var onlyValuesY = scene.map(function(obj){ return obj[activeScene+1]; });
+    var onlyValuesX = scene.map(function(obj){ return obj[1]; });
+
+    var minValueX = Math.min.apply(null, onlyValuesX),
+            maxValueX = Math.max.apply(null, onlyValuesX),
+            avgValueX = (minValueX + maxValueX)/2;
+
+    var minValueY = Math.min.apply(null, onlyValuesY),
+            maxValueY = Math.max.apply(null, onlyValuesY),
+            avgValueY = (minValueY + maxValueY)/2;
+
+    var paletteScale = d3.scaleLinear()
+        .domain([minValueX,avgValueX,maxValueX])
+        .range(series_pallet[0])
+    
+    scene.forEach(function(item){ //
+        var px = item[1],
+                py = item[activeScene+1],
+                country = item[0];
+        //data[iso] = { index: value, fillColor: paletteScale(value) };
+        data.push({x : px,y : py, fillColor: paletteScale(px), country : country  })
+    });
+
+    x_scatter.domain([minValueX*.95,maxValueX*1.05]);
+    y.domain([minValueY-.1*avgValueY,maxValueY+.1*avgValueY]);
+
+    // Add the Y Axis
+
+    svg_scatter.selectAll(".yAxis").remove()
+    
+    svg_scatter.append("g")
+        .attr("class","yAxis")
+        .call(d3.axisLeft(y));
+
+
+    var bars = svg_scatter.selectAll(".bar").data(data)
+    bars.exit().remove();
+
+    bars.enter()
+        .append('circle')
+        .attr("class", "bar")
+    bars.transition()
+        .duration(750)
+        .attr("r", 10)
+        .attr('fill', function(d,i){return d.fillColor})         
+        .attr("cx", function(d) { return x_scatter(d.x); })
+        .attr("cy", function(d) { return y(d.y); })
+        .on("mouseover", function(d) {
+            div.transition()		
+                .duration(200)		
+                .style("opacity", .9);
+            div.html("<strong>"+d.country+"</strong>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .style("display", "inline-block")
+        })
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0)	
+        });
+    bars.exit().remove();
+
+}
+
+updateScatterChart = function() {
+
+
+    $('#title').text(scenes[activeScene].name);
+    data = [];
+    
+    var onlyValuesY = scene.map(function(obj){ return obj[activeScene+1]; });
+    var onlyValuesX = scene.map(function(obj){ return obj[1]; });
+
+    var minValueX = Math.min.apply(null, onlyValuesX),
+            maxValueX = Math.max.apply(null, onlyValuesX),
+            avgValueX = (minValueX + maxValueX)/2;
+
+    var minValueY = Math.min.apply(null, onlyValuesY),
+            maxValueY = Math.max.apply(null, onlyValuesY),
+            avgValueY = (minValueY + maxValueY)/2;
+
+    var paletteScale = d3.scaleLinear()
+        .domain([minValueX,avgValueX,maxValueX])
+        .range(series_pallet[0])
+    
+    scene.forEach(function(item){ //
+        var px = item[1],
+                py = item[activeScene+1],
+                country = item[0];
+        //data[iso] = { index: value, fillColor: paletteScale(value) };
+        data.push({x : px,y : py, fillColor: paletteScale(px), country : country  })
+    });
+
+    x_scatter.domain([minValueX*.95,maxValueX*1.05]);
+    y.domain([minValueY-.1*avgValueY,maxValueY+.1*avgValueY]);
+
+    //var svg_scatter = svg.selectAll(".yAxis")
+    //.data(["dummy"]);
+
+    d3.select(".yAxis").remove();
+      // Add the X Axis
+    svg_scatter.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .attr("class","xAxis")
+        .call(d3.axisBottom(x_scatter));
+
+    // Add the Y Axis
+    svg_scatter.append("g")
+        .attr("class","yAxis")
+        .call(d3.axisLeft(y));
+
+
+    var bars = svg_scatter.selectAll(".bar")
+        .remove()
+        .exit()
+        .data(data)
+    bars.enter()
+        .append('circle')
+        .attr("class", "bar") 
+        .attr("r", 10)
+        .attr('fill', function(d,i){return d.fillColor})         
+        .attr("cx", function(d) { return x_scatter(d.x); })
+        .attr("cy", function(d) { return y(d.y); })
+        .on("mouseover", function(d) {
+            div.transition()		
+                .duration(200)		
+                .style("opacity", .9);
+            div.html("<strong>"+d.country+"</strong>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .style("display", "inline-block")
+        })
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0)	
+        });
+    bars.exit().remove();
+
+    dropData = createDropDownData();
+     
+    var options = select_scatter
+    .selectAll('option')
+      .data(dropData).enter()
+      .append('option')
+          .text(function (d) { return d.name; })
+          .attr('value', function(d) { return d.value;});
+
+}
+
+transitionBarChart = function() {
+    console.log('Transition');
+    data = [];
+    
+    var onlyValues = scene.map(function(obj){ return obj[activeScene+1]; });
+    var onlyNames = scene.map(function(obj) { return obj[0];})
+    var minValue = Math.min.apply(null, onlyValues),
+            maxValue = Math.max.apply(null, onlyValues),
+            avgValue = (minValue + maxValue)/2;
+
+    var paletteScale = d3.scaleLinear()
+        .domain([minValue,avgValue,maxValue])
+        .range(series_pallet[activeScene])
+    
+    scene.forEach(function(item){ //
+        var iso = item[0],
+                value = item[activeScene+1];
+        data.push({name : iso,index : value, fillColor: paletteScale(value) })
+    });
+
+    x.domain(data.map(function(d) { return d.name; }));
+    y.domain([minValue*.95,maxValue*1.05]);
+    
+    var y_axis = svg.selectAll(".yAxis")
+        .data(["dummy"]);
+    var new_y_axis = y_axis.enter().append("g")
+        .attr("class", "yAxis")
+
+    y_axis.merge(new_y_axis).call(d3.axisLeft(y)) 
+
+    var f = d3.format(",.2r")
+   
+    var bars = svg.selectAll(".bar").data(data)
+
+    bars.exit().remove();
+
+    bars.enter()
+        .append('rect')
+        .attr("class", "bar")
+    bars.transition()
+        .duration(750)         
+        .attr("x",  function(d,i) {return x(d.name); })
+        .attr("width", 10)
+        .attr("y", function(d,i){return y(d.index);})
+        .attr("height", function(d,i){return height-y(d.index);})
+        .attr('fill', function(d,i){return d.fillColor})
+
+    updateAnnotations(svg,activeScene,x,y);
+
+}
+
+updateBarChart = function() {
+    console.log('Update');
+    $('#title').text(scenes[activeScene].name);
+;
+
+    
+
+    data = [];
+    
+    var onlyValues = scene.map(function(obj){ return obj[activeScene+1]; });
+    var onlyNames = scene.map(function(obj) { return obj[0];})
+    var minValue = Math.min.apply(null, onlyValues),
+            maxValue = Math.max.apply(null, onlyValues),
+            avgValue = (minValue + maxValue)/2;
+
+    var paletteScale = d3.scaleLinear()
+        .domain([minValue,avgValue,maxValue])
+        .range(series_pallet[activeScene])
+    
+    scene.forEach(function(item){ //
+        var iso = item[0],
+                value = item[activeScene+1];
+        data.push({name : iso,index : value, fillColor: paletteScale(value) })
+    });
+
+    x.domain(data.map(function(d) { return d.name; }));
+    y.domain([minValue*.95,maxValue*1.05]);
+    
+
+    svg.append("g")
+        .attr("class", "xAxis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d){
+                return "rotate(-65)";
+            });
+
+    
+    var y_axis = svg.selectAll(".yAxis")
+            .data(["dummy"]);
+    var new_y_axis = y_axis.enter().append("g")
+        .attr("class", "yAxis")
+
+    y_axis.merge(new_y_axis).call(d3.axisLeft(y)) 
+
+    var t = d3.transition()
+        .duration(2000);
+
+    var f = d3.format(",.2r")
+
+    var bars = svg.selectAll(".bar")
+        .remove()
+        .exit()
+        .data(data)
+    bars.enter()
+        .append('rect')
+        .attr("class", "bar")        
+        .attr("x",  function(d,i) {return x(d.name); })
+        .attr("width", 10)
+        .attr("y", function(d,i){return y(d.index);})
+        .attr("height", function(d,i){return height-y(d.index);})
+        .attr('fill', function(d,i){return d.fillColor})
+        .on("mouseover", function(d) {
+            //console.log(d3.event.pageX,d3.event.pageY);
+            div.transition()		
+                .duration(200)		
+                .style("opacity", .9)
+            div.html("<strong>"+d.name+":</strong> <span style='color:red'>" + f(d.index) + "</span>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .style("display", "inline-block")
+        })
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0)	
+        });
+
+    updateAnnotations(svg,activeScene,x,y);
+
+    dropData = createDropDownData();
+    
+    var options = select_bar
+    .selectAll('option')
+      .data(dropData).enter()
+      .append('option')
+          .text(function (d) { return d.name; })
+          .attr('value', function(d) { return d.value;});
+}
+
+
+
+
+function setup_index() {
+    updateBarChart();
+}
+
+function setup_factors() {
+    activeScene = 1;
+    updateBarChart();
+}
+
+function setup_corrl() {
+    activeScene = 1;
+    updateScatterChart()
+}
+
+  /*
+
+
+              .on("mouseover", function(d) {
+                tooltip2
+                    .html("<strong>"+d.name+":</strong> <span style='color:red'>" + d.index + "</span>")
+                    .style("left", (d3.event.pageX - 50) + "px")
+                    .style("top", (d3.event.pageY - 70) + "px")
+                    .style("display", "inline-block");
+                })
+            .on("mouseout", function(d) {
+                tooltip2.transition()
+                    .style("opacity", 0);
+                });
+
+
+
+                    d3.select('#navigation').selectAll('button').data(scenes).enter()
+        .append('button')
+        .attr('type', 'button')
+        .attr('class', function (d, i) {
+        return "btn btn-primary" + (d.active ? " active" : "");
+        })
+        .attr('id', function (d, i) {
+            return 'btn-number'+d.number;
+        })
+        .text(function (d, i) {
+            return d.number;
+        })
+        .on("click", function(d,i) {
+            activeScene = scenes[i].number
+
+            showBlock();
+            updateChart();
+        })
+        .on("mouseover", function(d,i) {
+        tooltip.style("opacity", 1)
+            .style("left",(d3.event.pageX)+"px")
+            .style("top",(d3.event.pageY)+"px")
+            .html(d.name)
+            .style("display", "inline");
+
+            for (var i = 0; i < btns.length; i++) {
+                  var current = document.getElementsByClassName("btn-primary");
+                  current[0].className = current[0].className.replace(" active", "");
+            };
+        })
+        .on("mouseout", function(d){ tooltip.style("display", "none");});
+*/
